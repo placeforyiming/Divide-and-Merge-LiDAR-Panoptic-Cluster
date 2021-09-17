b184037b0cc3814f2ee1b837414c6fe680e2c61b
Yiming <emailfromyimingzhao@gmail.com>
2021-09-17 15:16:48 -0400

    add code

---
+++ evaluation_panoptic.sh
@@ -0,0 +1,2 @@
+/workspace/Divide-and-Conquer-LiDAR-Cluster/Dataset/semanticKITTI/semantic-kitti-api-master/evaluate_panoptic.py --dataset /workspace/Divide-and-Conquer-LiDAR-Cluster/Dataset/semanticKITTI/data_odometry_labels/dataset/  --predictions /workspace/Divide-and-Conquer-LiDAR-Cluster/method_predictions/ --split valid 
+
